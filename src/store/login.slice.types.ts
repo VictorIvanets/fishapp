031cc6d5948873d3edc38a5ad3299b70eab36980
unknown <imperia.zt@gmail.com>
2024-09-20 14:10:50 +0300

    fix bag2

--- src/store/login.slice.types.ts
+++ src/store/login.slice.types.ts
@@ -56,8 +56,8 @@ export interface RegisterResponse {
 	img: string
 }
 
-export const PREFIX = 'http://localhost:5550/api/'
-export const PREFIX_STATIC = 'http://localhost:5550/'
+// export const PREFIX = 'http://localhost:5550/api/'
+// export const PREFIX_STATIC = 'http://localhost:5550/'
 
-// export const PREFIX = 'https://fishapi.onrender.com/api/'
-// export const PREFIX_STATIC = 'https://fishapi.onrender.com/'
+export const PREFIX = 'https://fishapi.onrender.com/api/'
+export const PREFIX_STATIC = 'https://fishapi.onrender.com/'
